4dec028c283c5e1870f62add6714e22f8c0390f2
Parker Rhodes <p.rhodes@students.clark.edu>
2025-12-04 17:33:57 -0800

    Finished commenting the Rust code

--- rust_checkout/src/checkout/member.rs
+++ rust_checkout/src/checkout/member.rs
@@ -2,12 +2,16 @@
 
 
 pub struct Member {
+    //Member struct that has a name string and a vector of strings that store ids from a catalog
+    //struct
     name: String,
     current_ids: Vec<String>,
 }
 
 impl Member {
     pub fn new(name: String) -> Self {
+        //Give a name to the constructor and assign it to a new Member
+        //also construct a new vector
         Self {
             name: name,
             current_ids: vec![],
@@ -15,33 +19,47 @@ impl Member {
     }
 
     pub fn name(&self) -> String {
+        //Return a copy of the name string
         self.name.clone()
     }
 
     pub fn borrow(&mut self, id: String) {
+        //Add an id from a catalog to the current_ids vector
+        
+        //console output
         println!("Borrowing <{}> for User <{}>", id, self.name);
 
+        //check if member already has item borrowed
         if self.current_ids.contains(&id) {
             println!("User <{}> already has item <{}> borrowed!", self.name, id);
             return;
         }
 
+        //check if member has hit the borrow limit
         if self.current_ids.len() >= 5 {
             println!("User <{}> has the maximum items borrowed!", self.name);
             return;
         }
 
+        //else, add the id to the member's borrowed ids
         self.current_ids.push(id);
     }
 
     pub fn borrowed_ids(&self) -> Vec<String> {
+        //Return a copy of the current_ids vector
         self.current_ids.clone()
     }
 
     pub fn return_item(&mut self, id: String) {
+        //Remove an item from a user's current_ids vector
+
+        //console output
         println!("Returning <{}> for User <{}>", id, self.name);
 
+        //check if member actually has the id stored
         if self.current_ids.contains(&id) {
+            //if yes, iterate through the current_ids vector
+            //when the ids match, remove that id from the vector and break
             for i in 0..self.current_ids.len() {
                 if self.current_ids[i] == id {
                     self.current_ids.remove(i);
@@ -50,6 +68,7 @@ impl Member {
             }
         }
 
+        //if Member doesn't have the item borrowed, warn the user
         else {
             println!("Member <{}> does not have that Item borrowed!", self.name);
         }
--- rust_checkout/src/main.rs
+++ rust_checkout/src/main.rs
@@ -1,13 +1,15 @@
 //made by Parker Rhodes
 
+//bring in the data structures from the checkout folder
 mod checkout;
 use checkout::items::{Book, Dvd, Magazine};
 use checkout::catalog::Catalog;
 use checkout::member::Member;
 
 fn main() {
-    let mut cat=Catalog::new();
+    let mut cat=Catalog::new(); //consruct a new Catalog to store trait objects for the Item trait
 
+    //add a bunch of items to the catalog
     cat.add(Box::new(Book::new(String::from("B1"), String::from("Rust for Humans"))));
     cat.add(Box::new(Book::new(String::from("B2"), String::from("Pythonic Patterns"))));
     cat.add(Box::new(Book::new(String::from("B3"), String::from("Taking Flight"))));
@@ -15,19 +17,23 @@ fn main() {
     cat.add(Box::new(Dvd::new(String::from("D1"), String::from("Lord of the Flies"))));
     cat.add(Box::new(Magazine::new(String::from("M1"), String::from("Magic Code Mania"))));
 
+    //Construct a library member to borrow some items
     let mut clark=Member::new(String::from("Clark"));
     clark.borrow(String::from("B1"));
     clark.borrow(String::from("D1"));
     clark.borrow(String::from("B2"));
     clark.borrow(String::from("M1"));
 
+    //print currently borrowed items for Clark
     println!("{} has borrowed: {:?}", clark.name(), clark.borrowed_ids());
 
+    //print details for Clark's account in nice format
     println!("Printing details for Member <{}>", clark.name());
     for (id_val, title, days) in cat.details_for(&clark.borrowed_ids()) {
         println!("  -> {} - {} ({} days)", id_val, title, days);
     }
 
+    //return an item for Clark and list their items again
     clark.return_item(String::from("B1"));
     println!("{} has borrowed: {:?}", clark.name(), clark.borrowed_ids());
 }
@@ -40,6 +46,9 @@ mod tests {
 
     #[test]
     fn test_add_and_get() {
+        //setup_sample() adds some items to a test Catalog
+        //get returns an item from the Catalog, but currently works more like a 'pop' function,
+        //removing the item from the Catalog when retrieved
         let mut cat: Catalog=Catalog::new();
         cat.setup_sample();
 
@@ -52,6 +61,7 @@ mod tests {
 
     #[test]
     fn test_duplicate_id_rejected() {
+        //Ensure catalogs can only contain one of a given item
         let mut cat: Catalog=Catalog::new();
         cat.setup_sample();
 
@@ -64,6 +74,7 @@ mod tests {
 
     #[test]
     fn test_member_borrow_and_return() {
+        //ensure members can borrow items and return them once stored
         let mut cat: Catalog=Catalog::new();
         cat.setup_sample();
 
@@ -78,6 +89,7 @@ mod tests {
 
     #[test]
     fn test_member_cannot_borrow_twice() {
+        //Ensure Members cannot borrow more than one of the same item
         let mut m=Member::new(String::from("x"));
 
         m.borrow(String::from("K1"));
@@ -88,6 +100,7 @@ mod tests {
 
     #[test]
     fn test_member_limit_enforced() {
+        //ensure members can only store 5 items
         let mut m=Member::new(String::from("x"));
 
         for i in 0..6 {
@@ -100,6 +113,7 @@ mod tests {
 
     #[test]
     fn test_details_lines() {
+        //Ensure member details_for() output is functional
         let mut cat=Catalog::new();
         cat.setup_sample();
 
@@ -113,6 +127,7 @@ mod tests {
 
     #[test]
     fn test_member_name_and_ids() {
+        //Ensure member names and ids are constructed correctly
         let mut m=Member::new(String::from("x"));
 
         m.borrow(String::from("B1"));
@@ -123,6 +138,7 @@ mod tests {
 
     #[test]
     fn test_add_bad_item_should_pass() {
+        //pass a bad item to the Catalog and ensure the catalog rejects it
         let mut cat=Catalog::new();
 
         let bad_book=Box::new(Book::new(String::from(""), String::from("")));
